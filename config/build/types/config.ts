e7915503e83eab18f8fda523aed62c886f5b577c
Maxim Shtin <maksimshtin@gmail.com>
2025-09-26 20:39:29 +0500

    feat: adds dev server, uses env variables when building app

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -8,8 +8,14 @@ export interface BuildPaths {
 
 }
 
+export interface BuildEnv {
+    mode: BuildMode
+    port: number
+}
+
 export interface BuildOptions {
     mode: BuildMode
     paths: BuildPaths
     isDev: boolean
+    port: number
 }
